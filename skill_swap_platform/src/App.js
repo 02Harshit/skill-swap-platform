fc6df19f33fc14bb2d4563132fc64dd5130b104c
02Harshit <mittalharshit2004@gmail.com>
2025-07-12 16:23:49 +0530

    authentication system added

--- skill_swap_platform/src/App.js
+++ skill_swap_platform/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from "./pages/HomePage"
-import NavBar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from "./components/Navbar";
+import RegistrationPage from "./pages/RegistrationPage";
+import Home from "./pages/Home"; 
+import Pagination from "./components/Pagination";
+import SkillCard from "./components/SkillCard";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <NavBar /> 
+    <Router>
+      <Routes>
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
